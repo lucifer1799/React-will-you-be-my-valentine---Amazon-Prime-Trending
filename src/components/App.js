741347dec8cbe5c68b547f46681edfff1ce61d8a
lucifer1799 <47712039+lucifer1799@users.noreply.github.com>
2021-02-15 23:27:58 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,49 @@
-import React, {Component, useState} from "react";
+import React, {Component, useState,  useEffect} from "react";
 import '../styles/App.css';
 
+import slids from '../data'
 const App = () => {
+  const [i,setI]=useState(0);
+  const [preDesable,setPreDesable]=useState(false)
+  const [nextDesable,setnextDesable]=useState(false);
+
+  function handleNext(){
+    if(i<slids.length-1){
+      setI(i+1)
+    }
+
+  }
+  function handlePre(){
+    if(i>0){
+      setI(i-1)
+    }
+
+  }
+  useEffect(function(){
+    if(i<=0){
+      setPreDesable(true)
+    }
+    else{
+      setPreDesable(false)
+    }
+    if(i===slids.length-1){
+      setnextDesable(true)
+    }
+    else{
+      setnextDesable(false)
+    }
+  },[i])
+  function handelReset(){
+    setI(0);
+  }
   return (
-    <></>
-  )
+    <>
+      <h1 data-testid="title">{slids[i].title}</h1>
+      <p data-testid="text">{slids[i].text}</p>
+      <button onClick={handlePre} data-testid="button-prev" disabled={preDesable}>prev</button>
+      <button onClick={handleNext} data-testid="button-next" disabled={nextDesable}>next</button><br></br>
+      <button onClick={handelReset}>restart</button>
+    </>
+  );
 }
-
-
 export default App;
