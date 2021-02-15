e05e8f74d7a8c48b37a02eec05ac7b438cd64045
lucifer1799 <47712039+lucifer1799@users.noreply.github.com>
2021-02-16 00:13:49 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import React, {Component, useState,  useEffect} from "react";
 import '../styles/App.css';
 
-const App = ({slids}) => {
+const App = ({slides}) => {
   const [i,setI]=useState(0);
   const [preDesable,setPreDesable]=useState(false)
   const [nextDesable,setnextDesable]=useState(false);
 
   function handleNext(){
-    if(i<slids.length-1){
+    if(i<slides.length-1){
       setI(i+1)
     }
 
@@ -25,20 +25,20 @@ const App = ({slids}) => {
     else{
       setPreDesable(false)
     }
-    if(i===slids.length-1){
+    if(i===slides.length-1){
       setnextDesable(true)
     }
     else{
       setnextDesable(false)
     }
-  },[i,slids])
+  },[i,slides])
   function handelReset(){
     setI(0);
   }
   return (
     <div id="main">
-      <h1 data-testid="title" id="title">{slids[i].title}</h1>
-      <p data-testid="text" id="text">{slids[i].text}</p>
+      <h1 data-testid="title" id="title">{slides[i].title}</h1>
+      <p data-testid="text" id="text">{slides[i].text}</p>
       <button onClick={handlePre} data-testid="button-prev" id="button-prev" disabled={preDesable}>Prev</button>
       <button onClick={handleNext} data-testid="button-next" id="button-next" disabled={nextDesable}>Next</button>
       <button onClick={handelReset} data-testid="button-restart" id="button-restart" disabled={preDesable}>Restart</button>
